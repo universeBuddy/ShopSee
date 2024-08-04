adf3c2f90fcf7cb97c8965d75d7080ee85c9c5a4
universeBuddy <rai358788@gmail.com>
2024-08-04 20:48:27 +0530

    category route

--- Shopshe_Backend/dist/controllers/product.js
+++ Shopshe_Backend/dist/controllers/product.js
@@ -1,17 +1,34 @@
 import { TryCatch } from "../middlewares/error.js";
 import { Product } from "../models/ptoduct.js";
+import ErrorHandler from "../utility/utility-class.js";
+import { rm } from "fs";
 export const newProduct = TryCatch(async (req, res, next) => {
     const { name, price, stock, category } = req.body;
     const photo = req.file;
+    if (!photo)
+        return next(new ErrorHandler("Please Add Photo", 400));
+    if (!name || !price || !stock || !category) {
+        rm(photo.path, () => {
+            console.log("Deleted");
+        });
+        return next(new ErrorHandler("Please Add All Field", 400));
+    }
     await Product.create({
         name,
         price,
         stock,
         category: category.toLocaleLowerCase(),
-        photo: photo?.path,
+        photo: photo.path,
     });
     return res.status(201).json({
         success: true,
         message: "Product Created Successfully",
+    });
+});
+export const getlatestProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.find({}).sort({ ceratedAt: -1 }).limit(5);
+    return res.status(200).json({
+        success: true,
+        product,
     });
 });
--- Shopshe_Backend/src/routes/product.ts
+++ Shopshe_Backend/src/routes/product.ts
@@ -1,16 +1,18 @@
-import  express  from "express";
+import express from "express";
 import { newProduct } from "../controllers/product.js";
 import { adminOnly } from "../middlewares/auth.js";
 import { singleUpload } from "../middlewares/multer.js";
 
+import { getlatestProduct } from "../controllers/product.js";
 const app = express.Router();
- 
-// * create a New Product 
 
-app.post("/new",singleUpload ,newProduct)
- 
+// * create a New Product
+app.post("/new", adminOnly, singleUpload, newProduct);
 
+// * update a Product
+app.get("/latest", getlatestProduct);
 
+// * category route
 
 
 export default app;
